8afe90fe20ef2ed03fcceafd3bbfd95cb4848c0f
shifat-bin-reza <m.shifatbinreza@gmail.com>
2023-02-14 02:28:22 +0600

    fix some calculation bugs

--- js/script.js
+++ js/script.js
@@ -1,18 +1,16 @@
-document.getElementById("btn-login").addEventListener("click", function () {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-
-  if (email === "md.rudro1999@gmail.com" && password === "password") {
-    window.location.href = "dashboard.html";
-  } else {
-    const inputs = document.getElementsByClassName("input");
-    for (const input of inputs) {
-      input.style.borderColor = "red";
-      input.addEventListener("focusin", function () {
-        input.style.borderColor = "grey";
-      });
+document.getElementById("btn-login").addEventListener("click", function() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    if (email === "md.rudro1999@gmail.com" && password === "password") {
+        window.location.href = "dashboard.html";
+    } else {
+        const inputs = document.getElementsByClassName("input");
+        for (const input of inputs) {
+            input.style.borderColor = "red";
+            input.addEventListener("focusin", function() {
+                input.style.borderColor = "grey";
+            });
+        }
+        document.getElementById("invalid-info").style.display = "block";
     }
-
-    document.getElementById("invalid-info").style.display = "block";
-  }
 });
